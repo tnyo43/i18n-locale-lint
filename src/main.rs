b6418891d19f5d80bcaecb296523bdd1bd69c319
Tomoya Kashifuku <tnyo43@gmail.com>
2024-02-10 14:56:41 +0900

    add a base cli code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: my_cli_tool <arg1> <arg2>");
+        return;
+    }
+
+    let arg1 = &args[1];
+    let arg2 = &args[2];
+
+    println!("Argument 1: {}", arg1);
+    println!("Argument 2: {}", arg2);
 }
