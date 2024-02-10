b4596aeba237bc9d977e3aeee90ebe2fb0d263f2
Tomoya Kashifuku <tnyo43@gmail.com>
2024-02-10 20:45:00 +0900

    read json file and create data by parsing it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::env;
+use std::fs;
+use serde_json::{Result, Value};
+
+
+fn read_json() -> Result<()> {
+    let data = fs::read_to_string("data.json");
+    if let Ok(x) = data {
+        let parsed_data: Value = serde_json::from_str(&x)?;
+
+        println!("{:?}", parsed_data);
+    }
+
+    Ok(())
+}
 
 fn main() {
+    let _ = read_json();
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
