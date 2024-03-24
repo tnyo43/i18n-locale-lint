7ccf7eb0af2a067d78e780ef40cfcb5e66ade033
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-24 23:02:38 +0900

    Refactor (#12)
    * add diff module
    
    * move check function
    
    * move is_same_type function

--- src/check.rs
+++ src/check.rs
@@ -1,13 +1,22 @@
+use std::fs;
 use serde_json::Value;
 
-#[derive(Debug, PartialEq)]
-pub struct Diff<'a> {
-    pub key_to_value: Vec<String>,
-    pub expected: Option<&'a Value>,
-    pub actual: Option<&'a Value>,
+use crate::{diff::Diff, display, option};
+
+fn read_json_file(path: &str) -> Value {
+    let data = fs::read_to_string(path);
+    let content = if let Ok(content) = data {
+        content
+    } else {
+        "failed to parse".to_string()
+    };
+
+    let parsed_data: Value = serde_json::from_str(&content).unwrap();
+
+    parsed_data
 }
 
-pub fn is_same_type<'a>(
+fn is_same_type<'a>(
     base: &'a Value,
     target: &'a Value,
     key_to_value: &mut Vec<String>,
@@ -61,10 +70,45 @@ pub fn is_same_type<'a>(
     })
 }
 
+pub fn check(file_paths: &Vec<&str>) -> bool {
+    if file_paths.len() <= 1 {
+        return true;
+    }
+
+    if !option::INSTANCE.get().unwrap().silent {
+        println!("comparing:");
+        for path in file_paths {
+            println!("- {}", path);
+        }
+        println!();
+    }
+
+    let mut success = true;
+    let base_file_path = &file_paths[0];
+    let base_data = read_json_file(base_file_path);
+    for &target_file_path in file_paths.iter().skip(1) {
+        let target_data = read_json_file(target_file_path);
+        let result = is_same_type(&base_data, &target_data, &mut vec![]);
+        if let Some(diff) = result {
+            display::display_diff(&diff, base_file_path, target_file_path);
+            success = false;
+        }
+    }
+
+    if !option::INSTANCE.get().unwrap().silent {
+        if success {
+            println!("Ok!");
+        }
+        println!();
+    }
+
+    success
+}
+
 #[cfg(test)]
 mod tests {
     use serde_json::{Map, Value};
-    use crate::is_same_type::{is_same_type, Diff};
+    use crate::{check::is_same_type, diff::Diff};
 
     fn value_string() -> Value {
         "lorem".to_string().into()
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,11 @@
+mod check;
+mod diff;
 mod display;
 mod files;
-mod is_same_type;
 mod option;
 
 use std::env;
-use std::fs;
 use std::process::exit;
-use serde_json::Value;
-
-fn read_json_file(path: &str) -> Value {
-    let data = fs::read_to_string(path);
-    let content = if let Ok(content) = data {
-        content
-    } else {
-        "failed to parse".to_string()
-    };
-
-    let parsed_data: Value = serde_json::from_str(&content).unwrap();
-
-    parsed_data
-}
-
-fn check(file_paths: &Vec<&str>) -> bool {
-    if file_paths.len() <= 1 {
-        return true;
-    }
-
-    if !option::INSTANCE.get().unwrap().silent {
-        println!("comparing:");
-        for path in file_paths {
-            println!("- {}", path);
-        }
-        println!();
-    }
-
-    let mut success = true;
-    let base_file_path = &file_paths[0];
-    let base_data = read_json_file(base_file_path);
-    for &target_file_path in file_paths.iter().skip(1) {
-        let target_data = read_json_file(target_file_path);
-        let result = is_same_type::is_same_type(&base_data, &target_data, &mut vec![]);
-        if let Some(diff) = result {
-            display::display_diff(&diff, base_file_path, target_file_path);
-            success = false;
-        }
-    }
-
-    if !option::INSTANCE.get().unwrap().silent {
-        if success {
-            println!("Ok!");
-        }
-        println!();
-    }
-
-    success
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -67,7 +18,7 @@ fn main() {
     match files::get_file_groups(pattern) {
         Ok(file_groups) => {
             for group in file_groups {
-                ok &= check(&group.1.iter().map(|p| p.as_str()).collect());
+                ok &= check::check(&group.1.iter().map(|p| p.as_str()).collect());
             }
         }
         Err(e) => {
