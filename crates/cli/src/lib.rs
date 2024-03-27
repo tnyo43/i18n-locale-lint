71387477d31317ebbeb7db35d29988a88cb549ba
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-27 22:11:12 +0900

    add cli crate

---
+++ crates/cli/src/lib.rs
@@ -0,0 +1,30 @@
+use std::{env, process::exit};
+
+mod check;
+mod files;
+pub mod option;
+
+pub fn run() {
+    let args: Vec<String> = env::args().collect();
+    let pattern = args[1].as_str();
+
+    let option = option::Option::from_cli(&args);
+    let _ = option::INSTANCE.set(option);
+
+    let mut ok = true;
+    match files::get_file_groups(pattern) {
+        Ok(file_groups) => {
+            for group in file_groups {
+                ok &= check::check(&group.1.iter().map(|p| p.as_str()).collect());
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            ok = false;
+        }
+    }
+
+    if !ok {
+        exit(1);
+    }
+}
