f4b18e211734b33f312c1b38ab32dd2ea147618a
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-31 17:02:01 +0900

    update build.sh for release

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+TAG=""
+TARGET_DIR=debug
+if [ -n "${RELEASE}" ]; then
+  TAG="--release"
+  TARGET_DIR=release
+fi
+
+echo $TARGET_DIR build
+
 (
     cd crates/cli
-    cargo wasi build
+    cargo wasi build $TAG
 )
 mkdir -p wasm
-mv ./target/wasm32-wasi/debug/json-inspector-cli.wasm wasm/json-inspector-cli.wasm
+mv ./target/wasm32-wasi/$TARGET_DIR/json-inspector-cli.wasm wasm/json-inspector-cli.wasm
