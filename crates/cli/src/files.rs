20796763d70404731d78610b2cb1f8456acdef15
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-02 23:33:16 +0900

    update file passing method (#20)
    * fix to get file glob than glob string
    
    * fix main.mjs
    
    * fix examples
    
    * remove unnecessary dependency

--- crates/cli/src/files.rs
+++ crates/cli/src/files.rs
@@ -1,30 +1,6 @@
-use std::{collections::HashMap, error::Error, env};
+use std::collections::HashMap;
 use regex::Regex;
-
-#[cfg(not(target_os = "wasi"))]
-fn get_files() -> Result<Vec<String>, Box<dyn Error>> {
-    use glob::glob;
-    let args: Vec<String> = env::args().collect();
-    let pattern = args[1].as_str();
-
-    let mut files = Vec::new();
-    let paths = glob(pattern).expect("Failed to read glob pattern");
-
-    let x: Vec<Result<std::path::PathBuf, glob::GlobError>> = paths.collect();
-    for path in x {
-        files.push(path.unwrap().to_str().unwrap().to_string());
-    }
-
-    Ok(files)
-}
-
-#[cfg(target_os = "wasi")]
-fn get_files() -> Result<Vec<String>, Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let paths = args[0].split(",").map(|x| x.to_string()).collect();
-    println!("{:?}", paths);
-    Ok(paths)
-}
+use crate::option;
 
 fn group(file_paths: &Vec<String>) -> HashMap<String, Vec<String>> {
     let re = Regex::new(r"^(.*/)([^/]+)$").unwrap();
@@ -44,11 +20,9 @@ fn group(file_paths: &Vec<String>) -> HashMap<String, Vec<String>> {
     grouped_paths
 }
 
-pub fn get_file_groups() -> Result<HashMap<String, Vec<String>>, Box<dyn Error>> {
-    match get_files() {
-        Ok(file_paths) => Ok(group(&file_paths)),
-        Err(e) => Err(e),
-    }
+pub fn get_file_groups() -> HashMap<String, Vec<String>> {
+    let files = &option::INSTANCE.get().unwrap().files;
+    group(files)
 }
 
 #[cfg(test)]
