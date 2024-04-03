1eba64ba244643132824ce684f3382302f999e6f
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-04 08:40:03 +0900

    add tests

--- crates/json/src/parse.rs
+++ crates/json/src/parse.rs
@@ -22,3 +22,129 @@ pub fn convert(value: &serde_json::Value) -> Value {
         )),
     }
 }
+
+#[test]
+fn string_array() {
+    use insta::assert_snapshot;
+
+    let content = r#"
+{ 
+  "dayNames": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+}
+    "#
+    .to_string();
+
+    assert_snapshot!(parse(content),
+    @r#"{
+  "dayNames": [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ],
+}"#
+    );
+}
+
+#[test]
+fn object_with_types() {
+    use insta::assert_snapshot;
+
+    let content = r#"
+{
+  "currency": {
+    "format": "%{unit}%{number}",
+    "delimiter": ",",
+    "precision": 3,
+    "strip_insignificant_zeros": false,
+    "unit": "$"
+  }
+}
+    "#
+    .to_string();
+
+    assert_snapshot!(parse(content),
+    @r#"{
+  "currency": {
+    "delimiter": ",",
+    "format": "%{unit}%{number}",
+    "precision": 3,
+    "strip_insignificant_zeros": false,
+    "unit": "$",
+  },
+}"#
+    );
+}
+
+#[test]
+fn nested_object() {
+    use insta::assert_snapshot;
+
+    let content = r#"
+{
+  "date": {
+    "dayNames": ["Sun", "Mon", "..."],
+    "monthNames": [{ "full": "January", "short": "Jan" }, { "full": "February", "short": "Feb" }],
+    "formats": {
+      "default": "yyyy/MM/dd",
+      "monthDay": "MM/dd",
+      "yearMonth": "yyyy/MM",
+      "time": "hh:mm",
+      "full": "yyyy/MM/dd hh:mm"
+    }
+  },
+  "error": {
+    "format": "%{attribute} ${message}",
+    "messages": {
+      "too long": {
+        "one": "is too long (maximum is %{count} character)",
+        "other": "is too long (maximum is %{count} characters)"
+      }
+    }
+  }
+}
+    "#
+    .to_string();
+
+    assert_snapshot!(
+        parse(content),
+    @r#"{
+  "date": {
+    "dayNames": [
+      "Sun",
+      "Mon",
+      "...",
+    ],
+    "formats": {
+      "default": "yyyy/MM/dd",
+      "full": "yyyy/MM/dd hh:mm",
+      "monthDay": "MM/dd",
+      "time": "hh:mm",
+      "yearMonth": "yyyy/MM",
+    },
+    "monthNames": [
+      {
+        "full": "January",
+        "short": "Jan",
+      },
+      {
+        "full": "February",
+        "short": "Feb",
+      },
+    ],
+  },
+  "error": {
+    "format": "%{attribute} ${message}",
+    "messages": {
+      "too long": {
+        "one": "is too long (maximum is %{count} character)",
+        "other": "is too long (maximum is %{count} characters)",
+      },
+    },
+  },
+}"#
+    );
+}
