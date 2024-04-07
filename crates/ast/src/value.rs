258548284c720cccb2966b2a0c2a2977079b30ab
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-07 11:38:33 +0900

    add skip_top_level function

--- crates/ast/src/value.rs
+++ crates/ast/src/value.rs
@@ -15,6 +15,21 @@ pub enum Value {
     Map(HashMap<String, Box<Value>>),
 }
 
+impl Value {
+    pub fn skip_top_level(&self) -> Self {
+        if let Self::Map(map) = self {
+            if map.keys().len() != 1 {
+                panic!();
+            }
+            let key = map.keys().collect::<Vec<_>>()[0];
+
+            return *map.get(key).unwrap().clone();
+        } else {
+            panic!()
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Diff {
     pub key_to_value: Vec<String>,
