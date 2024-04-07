1fc34d55c027ac05e5bb07087b32c2af90a8eb84
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-07 11:11:39 +0900

    define option

--- crates/cli/src/option.rs
+++ crates/cli/src/option.rs
@@ -4,24 +4,41 @@ use once_cell::sync::OnceCell;
 pub struct Option {
     pub files: Vec<String>,
     pub silent: bool,
+    pub skip_top_level: bool,
 }
 pub static INSTANCE: OnceCell<Option> = OnceCell::new();
 
 impl Option {
     pub fn from_cli(args: &[String]) -> Self {
         let mut silent = false;
+        let mut skip_top_level = false;
 
         let mut opts = Options::new();
+
         opts.optflag("s", "silent", "no console output");
+        opts.optflag(
+            "",
+            "skip-top-level",
+            "assuming the top level is composed solely of a single key, and skipping it",
+        );
+
         let matches = match opts.parse(args) {
             Ok(m) => m,
             Err(f) => panic!("{}", f.to_string()),
         };
+
         if matches.opt_present("s") {
             silent = true;
         }
+        if matches.opt_present("skip-top-level") {
+            skip_top_level = true;
+        }
         let files = matches.free.clone();
 
-        Self { files, silent }
+        Self {
+            files,
+            silent,
+            skip_top_level,
+        }
     }
 }
