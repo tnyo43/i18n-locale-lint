347eedd3ca493fe02ab60de96d1629a5c6281fc3
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-07 12:51:46 +0900

    add help option

--- crates/cli/src/option.rs
+++ crates/cli/src/option.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use getopts::Options;
 use once_cell::sync::OnceCell;
 
@@ -8,13 +10,21 @@ pub struct Option {
 }
 pub static INSTANCE: OnceCell<Option> = OnceCell::new();
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] FILES", program);
+    print!("{}", opts.usage(&brief));
+}
+
 impl Option {
-    pub fn from_cli(args: &[String]) -> Self {
+    pub fn from_cli(raw_args: &[String]) -> Self {
         let mut silent = false;
         let mut skip_top_level = false;
 
         let mut opts = Options::new();
+        let program = &raw_args[0];
+        let args = &raw_args[1..];
 
+        opts.optflag("h", "help", "Print help menu");
         opts.optflag("s", "silent", "Don't display logs other than errors.");
         opts.optflag(
             "",
@@ -27,7 +37,11 @@ impl Option {
             Err(f) => panic!("{}", f.to_string()),
         };
 
-        if matches.opt_present("s") {
+        if matches.opt_present("help") {
+            print_usage(program, opts);
+            exit(0);
+        }
+        if matches.opt_present("silent") {
             silent = true;
         }
         if matches.opt_present("skip-top-level") {
