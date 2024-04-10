71f524bdcfe4c18e03658a8136b5d8b9d96948df
Tomoya Kashifuku <tnyo43@gmail.com>
2024-04-10 21:51:01 +0900

    add other than string keys

--- crates/yaml/src/parse.rs
+++ crates/yaml/src/parse.rs
@@ -8,6 +8,29 @@ pub fn parse(content: String) -> i18n_locale_lint_ast::value::Value {
     convert(&serde_value)
 }
 
+pub fn to_string(key: &serde_yaml::Value) -> String {
+    match key {
+        serde_yaml::Value::Null => "null".to_string(),
+        serde_yaml::Value::Bool(b) => b.to_string(),
+        serde_yaml::Value::Number(n) => n.to_string(),
+        serde_yaml::Value::String(s) => s.to_string(),
+        serde_yaml::Value::Tagged(t) => {
+            format!("{}:{}", &t.tag.to_string(), to_string(&t.value))
+        }
+        serde_yaml::Value::Sequence(arr) => format!(
+            "[{}]",
+            arr.iter().map(to_string).collect::<Vec<String>>().join(",")
+        ),
+        serde_yaml::Value::Mapping(obj) => format!(
+            "{{{}}}",
+            obj.iter()
+                .map(|(key, value)| format!("{}:{}", to_string(key), to_string(value)))
+                .collect::<Vec<String>>()
+                .join(",")
+        ),
+    }
+}
+
 fn convert(value: &serde_yaml::Value) -> Value {
     match value {
         serde_yaml::Value::String(s) => Value::Literal(Literal::String(s.clone())),
@@ -15,11 +38,10 @@ fn convert(value: &serde_yaml::Value) -> Value {
         serde_yaml::Value::Number(n) => Value::Literal(Literal::Number(n.as_f64().unwrap())),
         serde_yaml::Value::Null => Value::Literal(Literal::Null),
         serde_yaml::Value::Sequence(arr) => Value::Array(arr.iter().map(convert).collect()),
-        serde_yaml::Value::Mapping(obj) => {
-            Value::Map(HashMap::from_iter(obj.iter().map(|(key, value)| {
-                (key.as_str().unwrap().to_string(), Box::new(convert(value)))
-            })))
-        }
+        serde_yaml::Value::Mapping(obj) => Value::Map(HashMap::from_iter(
+            obj.iter()
+                .map(|(key, value)| (to_string(key), Box::new(convert(value)))),
+        )),
         serde_yaml::Value::Tagged(_) => todo!("yaml tag is not able to parse yet."),
     }
 }
@@ -105,6 +127,13 @@ date:
     yearMonth: yyyy/MM
     time: hh:mm
     full: yyyy/MM/dd hh:mm
+answer:
+  True: Yes
+  False: No
+  0: Zero
+  1: One
+  Null: Null
+!Tag 0: tag 0
 error:
   format: "%{attribute} ${message}"
   messages:
@@ -117,6 +146,14 @@ error:
     assert_snapshot!(
         parse(content),
     @r#"{
+  "!Tag:0": "tag 0",
+  "answer": {
+    "0": "Zero",
+    "1": "One",
+    "false": "No",
+    "null": null,
+    "true": "Yes",
+  },
   "date": {
     "dayNames": [
       "Sun",
