854d7ecf142b42696384f39453f4499e1929b450
Tomoya Kashifuku <tnyo43@gmail.com>
2024-05-06 16:31:38 +0900

    set exitCode after child process finish

--- bin/main.js
+++ bin/main.js
@@ -4,6 +4,11 @@ const path = require("node:path");
 const child_process = require("node:child_process");
 
 const binary_path = path.resolve(__dirname, "./executable/main");
-child_process.execFileSync(binary_path, process.argv.slice(2), {
+const result = child_process.spawnSync(binary_path, process.argv.slice(2), {
   stdio: "inherit",
 });
+if (result.error) {
+  throw result.error;
+}
+
+process.exitCode = result.status;
