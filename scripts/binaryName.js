88b79fd38e82335badd765adf1fd06b7805c2879
Tomoya Kashifuku <tnyo43@gmail.com>
2024-05-11 12:19:25 +0900

    check is musl when the platform is linux

--- scripts/binaryName.js
+++ scripts/binaryName.js
@@ -1,3 +1,4 @@
+const { execSync } = require("node:child_process");
 const { platform, arch } = process;
 
 const BIN_NAME_OF_PLATFORM = {
@@ -9,8 +10,25 @@ const BIN_NAME_OF_PLATFORM = {
     x64: "main-x86_64-unknown-linux-gnu",
     arm64: "main-aarch64-unknown-linux-gnu",
   },
+  "linux-musl": {
+    x64: "main-x86_64-unknown-linux-musl",
+    arm64: "main-aarch64-unknown-linux-musl",
+  },
 };
 
-const binName = BIN_NAME_OF_PLATFORM[platform]?.[arch];
+function isMusl() {
+  let stderr;
+  try {
+    execSync("ldd --version", { stdio: ["pipe", "pipe", "pipe"] });
+  } catch (err) {
+    stderr = err;
+  }
+  return stderr?.toString()?.indexOf("musl") > -1;
+}
+
+const binName =
+  platform === "linux" && isMusl()
+    ? BIN_NAME_OF_PLATFORM["linux-musl"]?.[arch]
+    : BIN_NAME_OF_PLATFORM[platform]?.[arch];
 
 exports.binName = binName;
