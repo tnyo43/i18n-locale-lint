e9a0af79e45175f2d44867bab2e0f7802ab51410
Tomoya Kashifuku <tnyo43@gmail.com>
2024-07-07 18:12:24 +0900

    chore: postinstall uses prepared asset list rather than fetching (#56)
    * chore: make asset list
    
    execute `curl https://api.github.com/repos/tnyo43/i18n-locale-lint/releases/tags/v0.3.3 | jq 'reduce .assets[] as $item ({}; .[$item.name] = $item.url)' > scripts/assets.json`
    
    * update postinstall script to use asset list

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,7 @@ const path = require("node:path");
 const https = require("follow-redirects/https");
 const { exit } = require("node:process");
 const { binName } = require("./binaryName");
+const assets = require("./assets.json");
 
 const PATH_EXECUTABLE = "bin";
 const PATH_EXECUTABLE_FILE = path.resolve(PATH_EXECUTABLE, binName);
@@ -14,44 +15,6 @@ if (!binName) {
     "This package doesn't ship with prebuilt binaries for your platform yet."
   );
   exit(1);
-}
-
-async function downloadAssetUrl() {
-  const result = await new Promise((resolve, reject) => {
-    const url = new URL(
-      `https://api.github.com/repos/tnyo43/i18n-locale-lint/releases/tags/v${DISTRIBUTION_VERSION}`
-    );
-    const hostname = url.hostname;
-    const path = url.pathname;
-    https
-      .request(
-        {
-          hostname,
-          path,
-          headers: {
-            Accept: "application/json",
-            "User-Agent": "@tnyo43/i18n-locale-lint",
-            "X-GitHub-Api-Version": "2022-11-28",
-          },
-        },
-        (res) => {
-          let data = "";
-          res.on("data", (chunk) => {
-            data += chunk;
-          });
-          res.on("end", () => {
-            resolve(JSON.parse(data));
-          });
-        }
-      )
-      .on("error", (e) => {
-        reject(e);
-      })
-      .end();
-  });
-
-  const asset = result.assets.find((value) => value.name === binName);
-  return asset.url;
 }
 
 async function downloadBinary(urlBase, filePath) {
@@ -102,7 +65,7 @@ async function downloadBinary(urlBase, filePath) {
 }
 
 async function main() {
-  const urlBase = await downloadAssetUrl();
+  const urlBase = assets[binName];
   await downloadBinary(urlBase, PATH_EXECUTABLE_FILE);
   fs.chmodSync(PATH_EXECUTABLE_FILE, "755");
 }
