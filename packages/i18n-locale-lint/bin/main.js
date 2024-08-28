e748325551bdd9689b98d02c43a35c986176ce84
Tomoya Kashifuku <tnyo43@gmail.com>
2024-08-28 22:26:43 +0900

    merge scripts

--- packages/i18n-locale-lint/bin/main.js
+++ packages/i18n-locale-lint/bin/main.js
@@ -1,13 +1,41 @@
 #!/usr/bin/env node
 
-const child_process = require("node:child_process");
-const { binName } = require("../scripts/binaryName");
+const { execSync, spawnSync } = require("node:child_process");
+const { platform, arch } = process;
 
-const result = child_process.spawnSync(
-  require.resolve(binName),
-  process.argv.slice(2),
-  { stdio: "inherit" }
-);
+const BIN_NAME_OF_PLATFORM = {
+  darwin: {
+    x64: "@i18n-locale-lint/cli-darwin-x64/main",
+    arm64: "@i18n-locale-lint/cli-darwin-arm64/main",
+  },
+  linux: {
+    x64: "@i18n-locale-lint/cli-linux-x64/main",
+    arm64: "@i18n-locale-lint/cli-linux-arm64/main",
+  },
+  "linux-musl": {
+    x64: "@i18n-locale-lint/cli-linux-x64-musl/main",
+    arm64: "@i18n-locale-lint/cli-linux-arm64-musl/main",
+  },
+};
+
+function isMusl() {
+  let stderr;
+  try {
+    execSync("ldd --version", { stdio: ["pipe", "pipe", "pipe"] });
+  } catch (err) {
+    stderr = err;
+  }
+  return stderr?.toString()?.indexOf("musl") > -1;
+}
+
+const binName =
+  platform === "linux" && isMusl()
+    ? BIN_NAME_OF_PLATFORM["linux-musl"]?.[arch]
+    : BIN_NAME_OF_PLATFORM[platform]?.[arch];
+
+const result = spawnSync(require.resolve(binName), process.argv.slice(2), {
+  stdio: "inherit",
+});
 if (result.error) {
   throw result.error;
 }
